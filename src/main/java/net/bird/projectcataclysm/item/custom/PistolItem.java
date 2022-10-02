c2cc34c01a0771affbef07473847374bd186fee3
jacksonD <jwd46122@gmail.com>
2022-10-02 18:03:09 -0400

    Added PistolItem w/ bow like attributes while debugging

---
+++ src/main/java/net/bird/projectcataclysm/item/custom/PistolItem.java
@@ -0,0 +1,57 @@
+package net.bird.projectcataclysm.item.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.*;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+import java.util.function.Predicate;
+
+public class PistolItem extends RangedWeaponItem {
+
+
+    public PistolItem(Item.Settings settings) { super(settings);}
+
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity playerEntity) {
+            ItemStack itemStack = playerEntity.getArrowType(stack);
+            if (itemStack.isEmpty()) {
+                itemStack = new ItemStack(Items.ARROW);
+            }
+
+            ArrowItem arrowItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.ARROW);
+            PersistentProjectileEntity persistentProjectileEntity = arrowItem.createArrow(world, itemStack, playerEntity);
+            persistentProjectileEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, 3.0F, 1.0F);
+
+            persistentProjectileEntity.setCritical(true);
+
+            world.spawnEntity(persistentProjectileEntity);
+        }
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        boolean bl = !user.getArrowType(itemStack).isEmpty();
+        if (!user.getAbilities().creativeMode && !bl) {
+            return TypedActionResult.fail(itemStack);
+        } else {
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(itemStack);
+        }
+    }
+
+
+
+    @Override
+    public Predicate<ItemStack> getProjectiles() {
+        return BOW_PROJECTILES;
+    }
+
+    @Override
+    public int getRange() {
+        return 15;
+    }
+}
