a8f249b71e7a9605383e2590c0812a6ad77e7207
Jackson <jackson.bounds@gmail.com>
2022-10-26 19:58:25 -0400

    Began work on the customExplosion class, some work with ray casting, need to work on affected entity collection.

--- src/main/java/net/bird/projectcataclysm/entity/custom/IceExplosiveEntity.java
+++ src/main/java/net/bird/projectcataclysm/entity/custom/IceExplosiveEntity.java
@@ -1,9 +1,12 @@
 package net.bird.projectcataclysm.entity.custom;
 
 import net.bird.projectcataclysm.entity.ModEntities;
+import net.bird.projectcataclysm.explosion.CustomExplosion;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.jetbrains.annotations.Nullable;
 
 public class IceExplosiveEntity extends ExplosiveEntity {
@@ -19,6 +22,27 @@ public class IceExplosiveEntity extends ExplosiveEntity {
     @Override
     public void explode() {
         // TODO Create ice effect
-    }
+        /*BlockPos positionClicked = context.getBlockPos();
+        int x = positionClicked.getX();
 
+        int y = positionClicked.getY() + 1;
+        int z = positionClicked.getZ();
+        BlockPos positionClicked2 = new BlockPos(x, y, z);
+        //positionClicked.add(100, 100, 100);
+        float dir = context.getPlayerFacing().asRotation();
+        PlayerEntity user = context.getPlayer();
+        //user.sendMessage(Text.literal(String.valueOf(dir)));
+        if (dir == 0 || dir == 180) {
+            for (int x2 = 0; x2 < 3; x2++) {
+                for (int y2 = 0; y2 < 5; y2++) {
+                    positionClicked = new BlockPos(x + x2, y + y2, z);
+                    context.getWorld().setBlockState(positionClicked, Blocks.PACKED_MUD.getDefaultState());
+                }
+                */
+        BlockPos position = new BlockPos(this.getPos());
+        CustomExplosion explosion = new CustomExplosion(this.world, this,
+                null, position.getX(), position. getY(), position.getZ(), 6);
+        explosion.collectBlocksAndDamageEntities();
+        explosion.affectWorld(true);
+    }
 }
